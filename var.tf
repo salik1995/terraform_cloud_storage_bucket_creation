350682514d253e0a60ff64f45c5ce29b5a515411
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-03-27 19:51:36 -0400

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,16 +1,16 @@
 variable "project_id" {
   description = "The ID of the GCP project."
-  default     = "avian-amulet-378416"
+  default     = "the-learning-project-378201"
 }
 
 variable "bucket_name" {
   description = "A list of unique names for the GCP storage buckets."
   type        = list(string)
   default     = [
-    "my-test-bucket-latest8999",
-    "mytestbucket8888",
-    "montrealcollege-927218-for-demo",
-    "bucket-full-of-happiness"
+    "mtlihokliyhatest8999",
+    "mytestbukljlcket8888",
+    "montrealcollege-9kjgkuhl27218-for-demo",
+    "tearnproject"
 
   ]
 }
